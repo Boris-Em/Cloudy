717b90398019cc6b5c7bc99fcb4a971d35605723
Boris Emorine <boris.emorine@gmail.com>
2016-07-29 19:06:02 -0700

    Create CloudyView.

---
+++ Cloudy/CloudyView.swift
@@ -0,0 +1,13 @@
+//
+//  CloudyView.swift
+//  CloudySampleProject
+//
+//  Created by Bobo on 7/29/16.
+//  Copyright © 2016 Boris Emorine. All rights reserved.
+//
+
+import UIKit
+
+class CloudyView: UIView {
+    
+}
