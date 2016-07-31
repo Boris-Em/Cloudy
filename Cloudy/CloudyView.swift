4aa825c4686408721f4d3d4ee40f878b276dd46d
Boris Emorine <boris.emorine@gmail.com>
2016-07-30 19:05:43 -0700

    Add reference to iOS in Cloudy description.

--- Cloudy/CloudyView.swift
+++ Cloudy/CloudyView.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-/** Random Cloudy Skies - Cloudy is a simple UIView subclass that lets you generate random yet beautiful clouds.
+/** Random Cloudy Skies for iOS - Cloudy is a simple UIView subclass that lets you generate random yet beautiful clouds.
  */
 @IBDesignable
 public class CloudyView: UIView {
